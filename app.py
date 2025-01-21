28949915f9adf85208c26ef56db2f4fcf52921cb
Hamadi camara <hamadi.camara@outlook.com>
2025-01-21 13:55:55 +0100

    Ajout du fichier script

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def addBook():
    return f'titre: {titre} | auteur : {auteur} | annee : {annee}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+   app.run(host='0.0.0.0', port=80)
